9f6442998db53c0a4430c73fa71b03e366ac9a3f
Chuka <chukaokakpu@gmail.com>
2020-05-08 18:55:37 -0400

    Work on Selectors

--- js/index.js
+++ js/index.js
@@ -8,6 +8,10 @@ const siteContent = {
     "nav-item-6": "Contact",
     "img-src": "img/logo.png"
   },
+  
+
+ 
+
   "cta": {
     "h1": "DOM Is Awesome",
     "button": "Get Started",
@@ -39,4 +43,41 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+//Links
+var aLinks = document.querySelectorAll('a');
+aLinks[0].textContent ="Services";
+aLinks[1].textContent = "Product";
+aLinks[2].textContent ="Vision";
+aLinks[3].textContent ="Features";
+aLinks[4].textContent ="About";
+aLinks[5].textContent ="Contact";
+
+// CTA Section
+var myH1 = document.querySelector('.cta h1');
+myH1.textContent = 'Dom is Awesome';
+myH1.style.flexWrap = 'wrap';
+var myBtn = document.querySelector('.cta button');
+myBtn.textContent = 'Get Started';
+var myImg = document.getElementById('cta-img');
+myImg.src = "img/header-img.png";
+
+// Middle Section
+//H4 Tag
+var allText = document.querySelectorAll('.text-content h4'); 
+allText[0].textContent = "Features";
+allText[1].textContent = "About";
+allText[2].textContent = "Services ";
+allText[3].textContent = "Product";
+allText[4].textContent = "Vision";
+//p Tag
+var allText = document.querySelectorAll('.text-content p'); 
+allText[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+allText[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';    
+allText[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+allText[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+allText[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+//img
+var midImg = document.getElementById("middle-img");
+midImg.src = 'img/mid-page-accent.jpg';
